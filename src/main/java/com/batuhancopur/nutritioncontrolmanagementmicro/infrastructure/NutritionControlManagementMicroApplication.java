8234781039abfd4d906581d766c133d5ba47e04d
Batuhan Çopur <batuhancopur@192.168.68.105>
2023-03-30 18:57:30 +0300

    Initial commit

---
+++ src/main/java/com/batuhancopur/nutritioncontrolmanagementmicro/infrastructure/NutritionControlManagementMicroApplication.java
@@ -0,0 +1,13 @@
+package com.batuhancopur.nutritioncontrolmanagementmicro.infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionControlManagementMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionControlManagementMicroApplication.class, args);
+	}
+
+}
